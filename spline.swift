4337d728dc3e48f084f18840a29aea61bfeb4acf
Jason Frame <jason@onehackoranother.com>
2016-02-21 00:33:21 +0000

    math constants

--- spline.swift
+++ spline.swift
@@ -3,7 +3,15 @@ import Cairo_swift
 
 prefix operator * {}
 
-public class Sketch2D {
+public class Constants {
+	public let QUARTER_PI = 0.7853982
+	public let HALF_PI = 1.57079632679489661923
+	public let PI = 3.14159265358979323846
+	public let TWO_PI = 6.28318530717958647693
+	public let TAU = 6.28318530717958647693
+}
+
+public class Sketch2D : Constants {
 	public init(width: Int, height: Int) {
 		
 		SDL.start()
